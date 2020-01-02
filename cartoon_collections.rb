fd47356da07233aecb88d3d6997be166d689e506
Shalva <shalvaa26@gmail.com>
2020-01-02 05:58:51 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -31,9 +31,11 @@ end
 long_planeteer_calls(planeteer_calls)
 
 potentially_cheesy_items = %w[umbrella spinach cheddar helicopter]
+
 def find_the_cheese# code an argument here
   # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
-  
-  
+  cheeses = %w[gouda cheddar camembert]
+
+  potentially_cheesy_items.find do |maybe_cheese|
+    cheeses.include?(maybe_cheese)
 end
