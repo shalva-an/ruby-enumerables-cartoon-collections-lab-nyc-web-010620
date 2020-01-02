069aad49d0e4ad5a880eca0968b7e83f2b27e50b
Shalva <shalvaa26@gmail.com>
2020-01-02 05:56:12 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -26,6 +26,7 @@ summon_captain_planet(planeteer_calls)
 
 def long_planeteer_calls(planeteer_calls)# code an argument here
   # Your code here
+  planeteer_calls.any? { |call| call.length > 4 }
 end
 
 def find_the_cheese# code an argument here
