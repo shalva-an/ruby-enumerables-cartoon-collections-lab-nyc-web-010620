c77cab0fe5995257b0a66027257e62fa425c6433
Shalva <shalvaa26@gmail.com>
2020-01-02 05:54:28 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,6 @@
 
 
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarves)# code an argument here
   # Your code here
   dwarves.each_with_index do |dwarf, index|
     puts "#{index + 1}. #{dwarf}"
